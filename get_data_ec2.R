04ac098006295ab14dd996bf857e2eaeb2bbc8c9
Ubuntu <ubuntu@ip-172-31-9-96.eu-west-2.compute.internal>
2019-04-10 08:23:30 +0000

    collect v2

--- get_data_ec2.R
+++ get_data_ec2.R
@@ -3,13 +3,14 @@ require(httr)
 require(rvest)
 filter = dplyr::filter
 
-setwd('~/Documents/data/gov.uk-international-announcements/')
+setwd('/home/ubuntu/gov.uk-international-announcements')
+
+# load('d.Rdata')
 
 # all international announcements
-u = 'https://www.gov.uk/government/announcements?announcement_filter_option=all&departments=all&from_date=&keywords=&page=%s&people=all&subtaxons=all&taxons=37d0fa26-abed-4c74-8835-b3b51ae1c8b2&to_date=&world_locations=all'
-
-
-u = 'https://www.gov.uk/government/announcements?keywords=&taxons=37d0fa26-abed-4c74-8835-b3b51ae1c8b2&subtaxons=all&announcement_filter_option=all&departments=all&people=all&world_locations=all&from_date=10%2F01%2F2019&to_date='
+# u = 'https://www.gov.uk/government/announcements?announcement_filter_option=all&departments=all&from_date=&keywords=&page=%s&people=all&subtaxons=all&taxons=37d0fa26-abed-4c74-8835-b3b51ae1c8b2&to_date=&world_locations=all'
+u0 = 'https://www.gov.uk/search/news-and-communications?level_one_taxon=37d0fa26-abed-4c74-8835-b3b51ae1c8b2&order=updated-newest&page='
+u1 = '&public_timestamp%5Bfrom%5D=&public_timestamp%5Bto%5D='
 
 # defence announcements (checks show all are included in 'international' category)
 # u = 'https://www.gov.uk/government/announcements?announcement_filter_option=all&departments=all&from_date=&keywords=&page=%s&people=all&subtaxons=all&taxons=e491505c-77ae-45b2-84be-8c94b94f6a2b&to_date=&world_locations=all'
@@ -17,27 +18,24 @@ u = 'https://www.gov.uk/government/announcements?keywords=&taxons=37d0fa26-abed-
 # DExEU - a very small number not included in international
 # u = 'https://www.gov.uk/government/announcements?announcement_filter_option=all&departments=department-for-exiting-the-european-union&from_date=&keywords=&page=%s&people=all&subtaxons=all&taxons=all&to_date=&world_locations=all'
 
-
 parse_item = function(item){
   item_url = html_node(item, 'a') %>% html_attr('href')
-  item_title = html_node(item, 'h3') %>% html_text %>% str_trim
-  item_ts = html_node(item, 'time.public_timestamp') %>% html_attr('datetime') %>% 
-    substr(1,19) %>% as.POSIXct(format='%Y-%m-%dT%H:%M:%S')
-  item_org = html_node(item, '.organisations') %>% html_text %>% str_trim
-  item_type = html_node(item, '.display-type') %>% html_text %>% str_trim
-  data_frame(published = item_ts, org = item_org, type = item_type, title = item_title, url = item_url)
+  item_title = html_node(item, 'a') %>% html_text %>% str_trim
+  item_ts = html_node(item, 'time') %>% html_attr('datetime') %>% substr(1,19) %>% as.Date()
+  item_org = html_node(item, '.metadata-text-value') %>% html_text %>% str_trim
+  tibble(published = item_ts, org = item_org, title = item_title, url = item_url)
 }
 
 # scrape the results pages
 
 dat = list()
 
-for(i in 1:6){
+for(i in 1:547){ #547
   message(i)
-  url = sprintf(u, as.character(i))
+  url = paste0(u0, i, u1)
   dom0 = GET(url)
   dom = read_html(dom0)
-  items = html_nodes(dom, '.document-row')
+  items = html_nodes(dom, '.document')
   dat[[i]] = map_df(items, parse_item)
 }
 
@@ -54,11 +52,13 @@ for(i in 1:nrow(d)){
   d$text[i] = html_node(dom, '#content') %>% html_text %>% str_replace_all('\n[ \t]+', '\n') %>% 
     str_replace_all('\n[ \t]+\n', '\n\n') %>% 
     str_replace_all('\n\n+', '\n') %>% str_remove('Share this page(.*\n)+')
+  if(is.na(d$org[i])) d$org[i] = html_node(dom, '.app-c-publisher-metadata__definition') %>% 
+    html_text %>% str_trim
 }
 
-# save(d, file = 'd.Rdata')
+save(d, file = 'd.Rdata')
+message('done')
 
-d %>% mutate(org = fct_lump(org, 9) %>% fct_infreq %>% fct_rev) %>% ggplot(aes(org)) + geom_bar() + coord_flip()
-d %>% mutate(type = fct_infreq(type) %>% fct_rev) %>% ggplot(aes(type)) + geom_bar() + coord_flip()
-d %>% mutate(week = lubridate::floor_date(published, 'week')) %>% ggplot(aes(week)) + geom_bar()
-
+# d %>% mutate(org = fct_lump(org, 9) %>% fct_infreq %>% fct_rev) %>% ggplot(aes(org)) + geom_bar() + coord_flip()
+# d %>% mutate(type = fct_infreq(type) %>% fct_rev) %>% ggplot(aes(type)) + geom_bar() + coord_flip()
+# d %>% mutate(week = lubridate::floor_date(published, 'week')) %>% ggplot(aes(week)) + geom_bar()
